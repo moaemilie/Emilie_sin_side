70b4a2d382e9ca6056748a0895a8ec41aaeae571
your_user_name_spelled_out <emilie.g.l@hotmail.com>
2026-01-15 16:49:33 +0100

    some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import CvContent from "@/components/CvContent";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full flex-col items-center bg-white sm:items-start">
-        <p className="text-2xl font-bold">
-          Dette er Emilie sin nettside hvor det skal bli vist hennes reise
-        </p>
+      <main className="flex min-h-screen w-full flex-col items-center sm:items-start">
+        <div className="w-full size-64 bg-foreground rounded-lg radius-50 p-8 mb-8">
+          <p className="text-2xl font-bold">
+            Dette er Emilie sin nettside hvor det skal bli vist hennes reise
+          </p>
+        </div>
         <CvContent />
       </main>
     </div>
