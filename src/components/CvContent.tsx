50b6bb1d56621f6cc7ae4d594d3258c24d283ac5
your_user_name_spelled_out <emilie.g.l@hotmail.com>
2026-01-17 00:19:19 +0100

    styling

--- src/components/CvContent.tsx
+++ src/components/CvContent.tsx
@@ -14,9 +14,9 @@ export default function CvContent() {
         }}
       >
         <div className="max-w-2xl mx-auto">
-          <h1 className="text-4xl font-bold mb-8 pixel-font text-gray-300">
+          {/*<h1 className="text-4xl font-bold mb-8 pixel-font text-gray-300">
             My CV
-          </h1>
+          </h1>*/}
 
           {sections.map((section: any) => (
             <div key={section.id} className="mb-12">
--- src/components/StartContent.tsx
+++ src/components/StartContent.tsx
@@ -1,8 +1,9 @@
 export default function StartContent() {
   return (
-    <div className="w-full size-64 bg-foreground rounded-lg radius-50 p-8 mb-8">
-      <p className="text-2xl font-bold">
-        Dette er Emilie sin nettside hvor det skal bli vist hennes reise
+    <div className="w-full size-64 bg-foreground/60 rounded-4xl p-8 mb-8 justify-center items-center flex flex-col gap-4">
+      <p className="text-4xl italic">
+        Hei! Jeg heter Emilie. Jeg er en kreativ utvikler med en lidenskap for å
+        skape.
       </p>
     </div>
   );
